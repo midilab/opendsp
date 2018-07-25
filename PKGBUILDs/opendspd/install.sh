19ec13b0d1b8e11bb2e62659e1ea91064328f40a
midilab <contact@midilab.co>
2018-07-25 11:21:30 -0300

    Added first packages, mostly audio related for now

---
+++ PKGBUILDs/opendspd/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+#opendspd install
+opendsp_passwd=opendsp
+
+useradd -m -G audio,video,uucp,lock,tty opendsp
+passwd opendsp <<EOF
+$(($opendsp_passwd))
+EOF
+
+# +depends
+# alsa: alsa-firmware alsa-lib alsa-plugins alsa-utils
+# x11: xf86-video-fbdev xorg-server xorg-xinit xorg-server-utils xterm dwm dmenu x11vnc
+# dev_env: base-devel python git premake3
+
+
+# X11 needs config for script auto start
+# config for normal users
+#sudo vi /etc/X11/Xwrapper.config
+#allowed_users = anybody
+#needs_root_rights = yes
+# xinitrc: exec dwm
