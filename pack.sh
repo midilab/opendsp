5a53131a2daa36851f49af9fc19ab2b85dbfa309
midilab <contact@midilab.co>
2018-08-07 10:54:28 -0300

    place all to be compiled packages on list

--- pack.sh
+++ pack.sh
@@ -2,31 +2,17 @@
 
 set -e
 
-#calf-git
-#drmr-falktx-git                 
-#lv2-git
-#rtirq     
-#fabla-git                        
-#mda-lv2-git  
-#midifilter.lv2-git  
-#projectm-jack
-#zam-plugins-git
-#distrho-lv2-git  
-#linux-raspberrypi-rt-opendsp  
-#ntk-git             
-#swh-lv2-git
-
-## declare an array variable
-declare -a package=("mididings-git" "ttymidi" "opendspd" "csound" "gmm" "suil-git" "ganv-git" "raul-git")
+# depends: base-devel git
+declare -a package=("linux-raspberrypi-rt-opendsp" "rtirq" "mididings-git" "ttymidi" "opendspd" "csound" "gmm" "suil-git" "ganv-git" "raul-git" "lv2-git" "ntk-git" "distrho-lv2-git" "midifilter.lv2-git" "calf-git" "fabla-git" "mda-lv2-git" "drmr-falktx-git" "swh-lv2-git" "zam-plugins-git" "projectm-jack")
 
 cd PKGBUILDs/
 
-## now loop through the above array
+
 for i in "${package[@]}"
 do
    cd "$i"
-   # or do whatever with individual element of the array
-   makepkg -isc
+   #makepkg -isc
+   makepkg -is
    cd ..
 done
 
