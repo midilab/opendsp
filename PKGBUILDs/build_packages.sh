f6b5ef5badd8d8f7773cfaacdfb84963eff4ed07
midilab <contact@midilab.co>
2018-09-25 10:25:19 -0300

    added calf-git and mda-lv2-git since main repo is not on lv2 latest changes we need to compile it by our selfs

--- PKGBUILDs/build_packages.sh
+++ PKGBUILDs/build_packages.sh
@@ -5,7 +5,7 @@ set -e
 sudo pacman -Syyu
 
 # depends: base-devel git cmake
-declare -a package=("mididings-git" "lv2-git" "ganv-git" "lilv-git" "raul-git" "serd-git" "suil-git" "ingen-git" "mod-ttymidi" "distrho-lv2-git" "midifilter.lv2-git" "fabla-git" "drmr-falktx-git" "swh-lv2-git" "zam-plugins-git" "dpf-plugins-git" "openav-luppp-git" "mixxx" "linux-raspberrypi-rt-opendsp" "opendspd")
+declare -a package=("mididings-git" "lv2-git" "ganv-git" "lilv-git" "raul-git" "serd-git" "suil-git" "ingen-git" "mod-ttymidi" "mda-lv2-git" "calf-git" "distrho-lv2-git" "midifilter.lv2-git" "fabla-git" "drmr-falktx-git" "swh-lv2-git" "zam-plugins-git" "dpf-plugins-git" "openav-luppp-git" "mixxx" "linux-raspberrypi-rt-opendsp" "opendspd")
 
 # lets get some memory space for compile process
 if [ ! -f "/swapfile" ]
@@ -21,7 +21,8 @@ for i in "${package[@]}"
 do
    cd "$i"
    echo "building $i"
-   makepkg -sci --noconfirm 
+   rm -rf src/ pkg/ *.tar.xz
+   makepkg -sci
    cp *.tar.xz ../../packages/armv7/ 
    cd ..
 done
