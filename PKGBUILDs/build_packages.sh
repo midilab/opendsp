61114d0e60e52083655c44c0f435111bf17f5e54
midilab <contact@midilab.co>
2020-06-08 07:24:41 -0400

    organize missing dependencies on archlinux repository to be compiled on our own

--- PKGBUILDs/build_packages.sh
+++ PKGBUILDs/build_packages.sh
@@ -6,8 +6,24 @@ sudo pacman -Syyu
 
 sudo pacman -S base-devel cmake git
 
-# depends: base-devel git cmake
-declare -a package=("mididings-git" "ganv-git" "raul-git" "ingen-git" "jamrouter-git" "mod-ttymidi" "distrho-lv2-git" "midifilter.lv2-git" "fabla-git" "drmr-falktx-git" "swh-lv2-git" "zam-plugins-git" "dpf-plugins-git" "openav-luppp-git" "mixxx" "opendspd")
+declare -a rt_kernels=("linux-raspberrypi-rt-opendsp" "linux-odroid-xu3-rt-opendsp")
+
+declare -a opendsp_base=("mididings-git" "mod-ttymidi" "jamrouter-git" "python-wiringpi-git" "input2midi" "novnc" "python-rtmidi" "opendspd")
+
+declare -a opendsp_audio=("raul-git" "ingen-git" "distrho-lv2-git" "midifilter.lv2-git" "non-daw-git" "klystrack-git" "milktracker-git" "dpf-plugins-git" "swh-lv2-git" "zam-plugins-git" "fabla-git" "drmr-falktx-git" "mod-sooper-looper-lv2")
+
+pack() {
+   echo "BUILDING PACKS..."
+   for i in "${1[@]}"
+   do
+      cd "$i"
+      echo "building $i"
+      rm -rf src/ pkg/ *.tar.xz
+      makepkg -sci
+      cp *.tar.xz ../../packages/armv7/ 
+      cd ..
+   done
+}
 
 # lets get some memory space for compile process
 if [ ! -f "/swapfile" ]
@@ -19,15 +35,10 @@ fi
 
 sudo swapon /swapfile || true
 
-for i in "${package[@]}"
-do
-   cd "$i"
-   echo "building $i"
-   rm -rf src/ pkg/ *.tar.xz
-   makepkg -sci
-   cp *.tar.xz ../../packages/armv7/ 
-   cd ..
-done
+# start pack
+#pack(rt_kernels)
+#pack(opendsp_base)
+pack(opendsp_audio)
 
 sudo swapoff -a
 sudo rm -f /swapfile
