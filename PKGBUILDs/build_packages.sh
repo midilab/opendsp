b3fcf261f7813494dfbb9c6dd953cbd4a0d36279
midilab <contact@midilab.co>
2020-06-09 09:23:20 -0400

    update list of non existent packages to build

--- PKGBUILDs/build_packages.sh
+++ PKGBUILDs/build_packages.sh
@@ -10,7 +10,9 @@ declare -a rt_kernels=("linux-raspberrypi-rt-opendsp" "linux-odroid-xu3-rt-opend
 
 declare -a opendsp_base=("mididings-git" "mod-ttymidi" "jamrouter-git" "python-wiringpi-git" "input2midi" "novnc" "python-rtmidi" "opendspd")
 
-declare -a opendsp_audio=("raul-git" "ingen-git" "distrho-lv2-git" "midifilter.lv2-git" "non-daw-git" "klystrack-git" "milktracker-git" "dpf-plugins-git" "swh-lv2-git" "zam-plugins-git" "fabla-git" "drmr-falktx-git" "mod-sooper-looper-lv2")
+declare -a opendsp_audio=("raul-git" "ingen-git" "distrho-lv2-git" "midifilter.lv2-git" "non-daw-git" "klystrack-git" "dpf-plugins-git" "swh-lv2-git" "zam-plugins-git" "fabla-git" "drmr-falktx-git" "mod-sooper-looper-lv2" "opendsp-audio-modular")
+
+declare -a opendsp_video=("sdl2-rpi" "mesa-rpi" "mesa-rpi-git" "lebiniou3" "lebiniou3-data" "processing" "omxplayer" "vlc" "puredata")
 
 pack() {
    local -n packs=$1
