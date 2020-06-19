bbcbdf8ddbce109e0c99008b4338debc7ba39bff
midilab <contact@midilab.co>
2020-06-19 07:52:31 -0400

    fix for symbolic link on files

--- PKGBUILDs/build_packages.sh
+++ PKGBUILDs/build_packages.sh
@@ -8,11 +8,11 @@ sudo pacman -S base-devel cmake git
 
 declare -a rt_kernels=("linux-raspberrypi-rt-opendsp" "linux-odroid-xu3-rt-opendsp")
 
-declare -a opendsp_base=("mididings-git" "mod-ttymidi" "jamrouter-git" "python-wiringpi-git" "input2midi" "novnc" "python-rtmidi" "opendspd" "opendsp-mods-factory")
+declare -a opendsp_base=("mididings-git" "mod-ttymidi" "jamrouter-git" "python-wiringpi-git" "input2midi" "novnc" "python-rtmidi" "opendspd" "opendsp-mods")
 
-declare -a opendsp_audio=("raul-git" "ingen-git" "distrho-lv2-git" "non-daw-git" "klystrack-git" "dpf-plugins-git" "swh-lv2-git" "zam-plugins-git" "drmr-falktx-git" "sunvox" "mod-sooper-looper-lv2" "opendsp-audio-modular")
+declare -a opendsp_audio=("raul-git" "ingen-git" "distrho-lv2-git" "non-daw-git" "klystrack-git" "dpf-plugins-git" "swh-lv2-git" "zam-plugins-git" "drmr-falktx-git" "sunvox" "mod-sooper-looper-lv2" "opendsp-mods-factory-audio" "opendsp-audio-modular")
 
-declare -a opendsp_video=("mesa-rpi" "mesa-rpi-git" "sdl2-rpi" "lebiniou3" "lebiniou3-data" "processing")
+declare -a opendsp_video=("mesa-rpi" "mesa-rpi-git" "sdl2-rpi" "lebiniou3" "lebiniou3-data" "processing" "opendsp-mods-factory-video" "opendsp-video-modular")
 
 pack() {
    local -n packs=$1
